f655d3fa331f911559766f79d43fbd55f966099f
Dmitry Karlinsky <dmitryka@wix.com>
2019-08-31 13:23:34 +0300

    Added test for LanguageList

--- src/main/java/com/xiaohansong/codemaker/ui/TargetLanguageSelect.kt
+++ src/main/java/com/xiaohansong/codemaker/ui/TargetLanguageSelect.kt
@@ -4,12 +4,13 @@ import com.intellij.lang.Language
 import javax.swing.JComboBox
 
 object TargetLanguageSelect {
+    private val languageList = LanguageList()
     @JvmStatic
     fun initCombo(langCombo: JComboBox<TargetLanguage>, selected: String) {
         // this sets the width of the combobox
         langCombo.prototypeDisplayValue = TargetLanguage("xxxxxxxxxxxxxx", "")
-        LanguageList.languages.forEach { langCombo.addItem(it) }
-        langCombo.selectedItem = LanguageList.find(selected)
+        LanguageList().languages.forEach { langCombo.addItem(it) }
+        langCombo.selectedItem = languageList.find(selected)
     }
 }
 
@@ -17,38 +18,41 @@ val JComboBox<TargetLanguage>.selectedLanguage: TargetLanguage
   get() = this.selectedItem as TargetLanguage
 
 
-class TargetLanguage(val name: String, val fileType: String) {
+data class TargetLanguage(val name: String, val fileType: String) {
     override fun toString(): String {
         return name
     }
 }
 
-object LanguageList {
-    val Language.fileType: String
-        get() = this.associatedFileType?.defaultExtension.orEmpty()
+class LanguageList(registeredLanguages: () -> Collection<Language> = Language::getRegisteredLanguages) {
 
-    val languages: List<TargetLanguage>
-    private val defaultLanguage = TargetLanguage("Java", "java")
+    val languages: List<TargetLanguage> by lazy {
+        val popularTypes = popularLanguges.map { it.fileType }.toSet()
+        popularLanguges + registeredLanguages()
+                .filter {
+                    it.fileType.isNotBlank() && !(it.fileType in popularTypes)
+                }
+                .sortedBy { it.displayName }
+                .map { TargetLanguage(it.displayName, it.associatedFileType?.defaultExtension.orEmpty()) }
+    }
 
     fun find(id: String): TargetLanguage =
-        languages.find { it.fileType.toLowerCase() == id.toLowerCase() } ?: defaultLanguage
+            languages.find { it.fileType.toLowerCase() == id.toLowerCase() } ?: defaultLanguage
 
-    init {
-        val popular = listOf(
+
+
+    companion object {
+        val Language.fileType: String
+            get() = this.associatedFileType?.defaultExtension.orEmpty()
+
+        val defaultLanguage = TargetLanguage("Java", "java")
+
+        val popularLanguges = listOf(
                 defaultLanguage,
                 TargetLanguage("Scala", "scala"),
                 TargetLanguage("Kotlin", "kt"),
                 TargetLanguage("JavaScript", "js"),
                 TargetLanguage("TypeScript", "ts"),
                 TargetLanguage("SQL", "sql"))
-
-        val popularTypes = popular.map { it.fileType }.toSet()
-
-        languages = popular + Language.getRegisteredLanguages()
-                .filter {
-                    it.fileType.isNotBlank() && !(it.fileType in popularTypes)
-                }
-                .sortedBy { it.displayName }
-                .map { TargetLanguage(it.displayName, it.associatedFileType?.defaultExtension.orEmpty()) }
     }
 }
